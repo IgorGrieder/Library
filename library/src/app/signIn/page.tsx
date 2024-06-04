31879542a8c692aaf1d5a45a5d026021e76a0f85
Igor Grieder Melo <igorgrieder@gmail.com>
2024-06-04 18:08:59 -0300

    Changing the way to persist user information

--- library/src/app/signIn/page.tsx
+++ library/src/app/signIn/page.tsx
@@ -1,18 +1,17 @@
 'use client';
 import { userCtx } from '@/context/userContext';
 import { useRouter } from 'next/navigation';
-import { FormEvent, useContext, useState } from 'react';
+import { ChangeEvent, FormEvent, useContext, useState } from 'react';
 
 const SignIn = () => {
-  const [userInput, setUserInput] = useState(''); // State variable to control the user
-  const [passwordInput, setPasswordInput] = useState(''); // State variable to control the password
+  const [userInput, setUserInput] = useState({ name: '', password: '' }); // State object to control the user and the password input
   const userContext = useContext(userCtx); // Importing the user context
   const router = useRouter(); // Creating an instance of router
 
   // Function to handle the submit process on the form
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (userInput !== '' && passwordInput !== '') {
+    if (userInput.name !== '' && userInput.password !== '') {
       userContext?.setUser({
         name: 'Igor',
         age: 20,
@@ -22,6 +21,16 @@ const SignIn = () => {
     }
   };
 
+  // Function to handle chagens on the inputs
+  const handleInputs = (e: ChangeEvent<HTMLInputElement>) => {
+    const eTarget = e.target.name; // Getting the name of the event target input element
+    setUserInput({
+      // cloning the object and changing the info dinamically for the specific field
+      ...userInput,
+      [eTarget]: e.target.value,
+    });
+  };
+
   return (
     <div className="text-black bg-white h-screen">
       <form onSubmit={handleFormSubmit}>
@@ -29,16 +38,18 @@ const SignIn = () => {
           <span>User</span>
           <input
             type="text"
+            name="User"
             placeholder="User"
-            onChange={(e) => setUserInput(e.target.value)}
+            onChange={handleInputs}
           />
         </label>
         <label>
           <span>Password</span>
           <input
             type="password"
+            name="Password"
             placeholder="Password"
-            onChange={(e) => setPasswordInput(e.target.value)}
+            onChange={handleInputs}
           />
         </label>
         <button>Log in</button>
